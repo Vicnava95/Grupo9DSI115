f9a30a2539911f55b96aca97373f2ac4bf45b2bf
Vicnava95 <47409039+Vicnava95@users.noreply.github.com>
2021-05-17 19:56:13 -0600

    Primer commit

---
+++ Grupo9DSI115/resources/views/Prueba/prueba.blade.php
@@ -0,0 +1,21 @@
+@extends('Master.master')
+
+@section('title')
+Prueba
+@stop
+
+@section('extraJS')
+<script src="js/master.js"></script>
+@stop
+
+@section('extraCSS')
+<link href="css/master.css" rel="stylesheet" type="text/css"/>
+@stop
+
+@section('tituloTemplate')
+Prueba
+@stop
+
+@section('contenido')
+<h1>Todo el contenido</h1>
+@stop
