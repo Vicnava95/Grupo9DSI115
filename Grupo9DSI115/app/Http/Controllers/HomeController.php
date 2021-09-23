4017a0a2efca6e0032bd0195aa5267dfb2a6b8b4
Vicnava95 <venm95@hotmail.com>
2021-09-22 19:58:44 -0600

    cambios al inicio de sesión

--- Grupo9DSI115/app/Http/Controllers/HomeController.php
+++ Grupo9DSI115/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        switch(Auth::user()->rols_fk){
+            case 1:
+                return redirect()->route('dshAdministrador.index');
+                break;
+            case 2:
+                return redirect()->route('dshDoctorGeneral.index');
+                break;
+            case 3:
+                return redirect()->route('dshDoctorDental.index');
+                break;
+            case 4;
+                return redirect()->route('dshSecretaria.index');
+                break;
+            default:
+                return view('home');
+        }
+        
     }
 }
